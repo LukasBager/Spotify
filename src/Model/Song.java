4aa1f5100a19f698b29eb4aa15a46ab9a975b378
CilleElisabeth <cime1000@stud.ek.dk>
2026-03-04 12:40:15 +0100

    Song klasse startet

--- src/Model/Song.java
+++ src/Model/Song.java
@@ -1,4 +1,32 @@
 package Model;
 
 public class Song {
-}
+    private String title;
+    private String artist;
+    private Genre genre; //musikgenre fra enum
+
+    public Song(String title, String artist, Genre genre) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+    }
+        //getters
+        public String getTitle() {
+            return title;
+        }
+        public String getArtist() {
+            return artist;
+        }
+        public Genre getGenre() {
+            return genre;
+        }
+//setter så man kan ændre titlen
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        @Override
+                public String toString() {
+            return title + " - " + artist + " (" + genre + ")";
+        }
+    }
