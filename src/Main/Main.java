9ea5df1073aa61ef1e2ff9b8a77161b7734bdf52
LukasBager <lukasemilbager@protonmail.com>
2026-03-06 10:09:01 +0100

    Print menu added i main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,7 +3,18 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
+        printMenu();
+    }
 
+    public static void printMenu() {
+        System.out.println("Velkommen til dit hjemmelavede Spotify! Vælg en mulighed:");
+        System.out.println("1. Tilføj ny sang");
+        System.out.println("2. Fjern en sang");
+        System.out.println("3. Vis alle sang");
+        System.out.println("4. Søg efter en sang");
+        System.out.println("5. Rediger en sang");
+        System.out.println("6. Sort sanglisten.");
+        System.out.println("7. Afslut programmet");
     }
 
 }
